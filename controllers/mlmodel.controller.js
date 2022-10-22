b2047fcd7f3e04d30f965a14ee9ea4d61d088a05
ageraustine <agerausten@gmail.com>
2022-10-22 13:37:47 +0300

    decode color results

--- controllers/mlmodel.controller.js
+++ controllers/mlmodel.controller.js
@@ -1,17 +1,20 @@
-const createError = require('http-errors')
-const loadModel = require('../utilities/loadModel')
-const preprocess = require('../utilities/preprocess')
+const createError = require('http-errors');
+const decodePrediction = require('../utilities/decodePredictions');
+const loadModel = require('../utilities/loadModel');
+const preprocess = require('../utilities/preprocess');
  
 class MlModelController{
     static async getColor(req, res, next){
       try{
-        const imageBuffer = req.file.buffer
-        const model = await loadModel()
-        const imageArray = await preprocess(imageBuffer)
-        const predictions = await model.predict(imageArray)
+        const imageBuffer = req.file.buffer;
+        const model = await loadModel();
+        const imageArray = await preprocess(imageBuffer);
+        const predictions = await model.predict(imageArray);
+        const color = decodePrediction(predictions[0]);
+        res.status(200).json({"color":color});
       }
       catch(e){
-        next(createError(e.statusCode, e.message))
+        next(createError(e.statusCode, e.message));
       }  
     }
 }
